b5e34fb7d1889474560f4ba0445a9e00e14f05da
alicjapisk <alicjapisk@gmail.com>
2025-10-02 14:05:15 +0200

    fix button

--- src/app/components/add-habit/AddHabitButton.tsx
+++ src/app/components/add-habit/AddHabitButton.tsx
@@ -1,3 +1,5 @@
+import { Text } from "../Text";
+
 interface Props {
   habitName: string;
   selectedColor: string;
@@ -7,10 +9,12 @@ const AddHabitButton = ({ selectedColor, habitName, selectedEmoji }: Props) => {
   return (
     <button
       type="submit"
-      className={`w-full bg-gradient-to-r ${selectedColor} hover:opacity-90 text-white px-6 py-4 rounded-2xl disabled:opacity-50 disabled:hover:scale-100 shadow-lg transform hover:scale-105 transition-all duration-200 font-medium`}
+      className={`w-full bg-gradient-to-r ${selectedColor} hover:opacity-90 px-6 py-4 rounded-2xl disabled:opacity-50 disabled:hover:scale-100 shadow-lg transform hover:scale-105 transition-all duration-200`}
       disabled={!habitName.trim()}
     >
-      {selectedEmoji} Add habit
+      <Text variant="T1" className="text-white">
+        {selectedEmoji} Add habit
+      </Text>
     </button>
   );
 };
